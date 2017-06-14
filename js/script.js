fe0deae1b242bc32d9513ebb200ac817d2165ea8
Valter Luiz <Valter Luiz>
2017-06-14 16:14:01 -0300

    Site Placeholder Completo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-var animFrame = []
+var animFrames = ["img/logoAnim/frame0.jpg","img/logoAnim/frame1.jpg","img/logoAnim/frame2.jpg"];
+
+var frame = 0;
+var frameSrc = "";
+
+function changeFrame(){
+    frameSrc = animFrames[frame];
+
+    document.getElementById("logo").src = frameSrc;
+
+    frame++;
+
+    if(frame == 3){frame = 0};
+}
+
+setInterval(changeFrame, 1000);
+
+//document.getElementById("logo").src = "img/logoAnim/frame0.jpg";
